556fad3d62970fe73e9708813a5ed0ea43fab46d
Valentina Vențel <valentina.ventel@cobaltsign.com>
2020-10-23 12:48:59 +0300

    initial commit

---
+++ Cereri_CobaltSign/Views/ViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cereri_CobaltSign
+//
+//  Created by Valentina Vențel on 23/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
